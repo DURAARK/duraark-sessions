d87b57120df1e1edc3af9f025ef1e85f9b4931c0
Martin Hecher <martin.hecher@fraunhofer.at>
2015-02-09 11:03:46 +0100

    unified scripts and deployment into a 'devops' folder

--- _devops/serve-prod.sh
+++ _devops/serve-prod.sh
@@ -6,4 +6,6 @@ SERVICENAME=$(cat ./service-info.txt)
 INDEXFILE="app.js"
 FOLDER="../"
 
-(cd $FOLDER; pm2 delete $SERVICENAME; pm2 start $INDEXFILE -x --name $SERVICENAME -- --prod)
+#(cd $FOLDER; pm2 delete $SERVICENAME; pm2 start $INDEXFILE -x --name $SERVICENAME -- --prod)
+#pm2 logs
+(cd $FOLDER; nodemon)
