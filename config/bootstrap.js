7629d885f5418c4a5ea0c819d091e7dd8ac4a9a3
Martin Hecher <martin.hecher@fraunhofer.at>
2015-02-12 20:57:42 +0100

    refactored 'sessions' fixtures into persistent models

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,19 +9,56 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
+var _ = require('underscore');
+
 module.exports.bootstrap = function(cb) {
 
-	// User.find({
-	// 	admin: true
-	// }).exec(function(err, adminUser) {
-	// 	// If an admin user exists, skip the bootstrap data
-	// 	if (adminUser) {
-	// 		return cb();
-	// 	}
-	// 	// Otherwise, create data below...
-	// });
+	Sessions.find({
+		name: 'Inffeldgasse 16c'
+	}).exec(function(err, sessions) {
+		console.log('seASDFASDFASDF: ' + JSON.stringify(sessions, null, 4));
 
-	// It's very important to trigger this callback method when you are finished
-	// with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-	cb();
+		if (sessions.length) {
+			console.log('Fixtures already in place, skipping creation.');
+			return cb();
+		} else {
+
+			console.log('Creating fixtures ...');
+
+			var _sessions = [{
+				name: 'Inffeldgasse 16c',
+				creator: 'Martin Hecher',
+				created: new Date(),
+				filestage: 0
+			}, {
+				name: 'Power Socket Scan',
+				creator: 'Martin Hecher',
+				created: new Date(),
+				filestage: 1
+			}];
+
+			_.forEach(_sessions, function(session) {
+				console.log('foreach: ' + session);
+				Sessions.create(session).exec(function(err, record) {
+					if (err) {
+						console.log('err create: ' + err);
+					} else {
+						console.log('created: ' + JSON.stringify(record, null, 4));
+
+						record.save(function(err) {
+							if (err) {
+								console.log('save err: ' + err);
+							}
+						});
+					}
+				});
+			});
+
+			console.log('   done');
+		}
+
+		// It's very important to trigger this callback method when you are finished
+		// with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+		cb();
+	});
 };
