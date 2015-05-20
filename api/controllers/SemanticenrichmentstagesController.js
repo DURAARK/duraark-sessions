b7792ae185e9723f08245697f78be9021a7b24fe
martinhecher <martin.hecher@fraunhofer.at>
2015-05-20 18:24:06 +0200

    remaining fixes for last commit

--- api/controllers/SemanticenrichmentstagesController.js
+++ api/controllers/SemanticenrichmentstagesController.js
@@ -11,8 +11,8 @@ function store(req, res, next) {
     var payload = JSON.parse(req.param('json'));
     var availableItems = payload.availableItems;
     var selectedItems = payload.selectedItems;
-    var session = req.param('session'),
-        name = req.param('name'),
+    var session = payload.session,
+        name = payload.name,
         available = [],
         selected = [];
 
@@ -23,6 +23,7 @@ function store(req, res, next) {
         name: name
     };
 
+    console.log('session: ' + session);
     console.log('selected/available: ' + availableItems.length + ' / ' + selectedItems.length);
 
     Semanticenrichmentstages.findOrCreate({
@@ -48,10 +49,12 @@ function store(req, res, next) {
 
 module.exports = {
     create: function(req, res, next) {
+        console.log('create');
         store(req, res, next);
     },
 
     update: function(req, res, next) {
+        console.log('update');
         store(req, res, next);
     }
 };
