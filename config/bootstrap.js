d8ba0aa61e9da89940eb07bd1789f2085518f417
martinhecher <martin.hecher@fraunhofer.at>
2015-11-04 12:37:02 +0100

    added fixed sessions

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -266,8 +266,7 @@ var sessions = [{
     updatedAt: "2015-07-31T14:20:40.432Z",
     id: 2
   }]
-},
-{
+}, {
   state: 'archived',
   label: 'Nygade',
   address: 'Nygade Straat, Kopenhagen',
@@ -295,7 +294,7 @@ var sessions = [{
     ctime: "2015-07-30T16:18:51.000Z",
     createdAt: "2015-07-31T14:20:40.373Z",
     updatedAt: "2015-07-31T14:20:40.402Z",
-    id: 1
+    id: 3
   }, {
     path: "/duraark-storage/sessions/nygade1005-1006-fixed/master/Nygade_Scan1005-1006.ifc",
     type: "ifc-spf",
@@ -306,9 +305,9 @@ var sessions = [{
     ctime: "2015-07-30T16:18:51.000Z",
     createdAt: "2015-07-31T14:20:40.378Z",
     updatedAt: "2015-08-02T09:00:34.781Z",
-    id: 2
+    id: 4
   }]
-},{
+}, {
   state: 'archived',
   label: 'Bygade 72',
   address: 'Somewhere, Kopenhagen',
@@ -336,6 +335,156 @@ var sessions = [{
     ctime: "2015-07-30T16:18:51.000Z",
     createdAt: "2015-07-31T14:20:40.384Z",
     updatedAt: "2015-08-02T09:00:41.042Z",
-    id: 1
+    id: 5
+  }]
+}, {
+  state: 'archived',
+  label: 'Lyslab 03',
+  address: 'Somewhere, Kopenhagen',
+  description: 'Reconstruction',
+
+  physicalAssets: [],
+  digitalObjects: [],
+
+  config: {
+    sda: {
+      topics: ["Haus 30 (general context)", "Haus 30 (political context)"]
+    },
+    geometricenrichment: {
+      tools: ["IFC Reconstruction", "Electrical Appliance Detection"]
+    }
+  },
+
+  files: [{
+    path: "/duraark-storage/sessions/byg72-2nd-scan_fixed/master/LysLab_Scan03.e57",
+    type: "e57",
+    size: 1071950848,
+    directory: false,
+    atime: "2015-07-30T16:16:10.000Z",
+    mtime: "2015-07-30T16:16:15.000Z",
+    ctime: "2015-07-30T16:18:51.000Z",
+    createdAt: "2015-07-31T14:20:40.384Z",
+    updatedAt: "2015-08-02T09:00:41.042Z",
+    id: 6
+  }]
+}, {
+  state: 'archived',
+  label: 'Nygade Scan37',
+  address: 'Somewhere, Kopenhagen',
+  description: 'Reconstruction',
+
+  physicalAssets: [],
+  digitalObjects: [],
+
+  config: {
+    sda: {
+      topics: ["Haus 30 (general context)", "Haus 30 (political context)"]
+    },
+    geometricenrichment: {
+      tools: ["IFC Reconstruction", "Electrical Appliance Detection"]
+    }
+  },
+
+  files: [{
+    path: "/duraark-storage/sessions/byg72-2nd-scan_fixed/master/Nygade_Scan037.e57",
+    type: "e57",
+    size: 268553216,
+    directory: false,
+    atime: "2015-07-30T16:16:10.000Z",
+    mtime: "2015-07-30T16:16:15.000Z",
+    ctime: "2015-07-30T16:18:51.000Z",
+    createdAt: "2015-07-31T14:20:40.384Z",
+    updatedAt: "2015-08-02T09:00:41.042Z",
+    id: 7
+  }]
+}, {
+  state: 'archived',
+  label: 'Nygade 1001',
+  address: 'Somewhere, Kopenhagen',
+  description: 'Reconstruction',
+
+  physicalAssets: [],
+  digitalObjects: [],
+
+  config: {
+    sda: {
+      topics: ["Haus 30 (general context)", "Haus 30 (political context)"]
+    },
+    geometricenrichment: {
+      tools: ["IFC Reconstruction", "Electrical Appliance Detection"]
+    }
+  },
+
+  files: [{
+    path: "/duraark-storage/sessions/byg72-2nd-scan_fixed/master/Nygade_Scan1001.e57",
+    type: "e57",
+    size: 270408704,
+    directory: false,
+    atime: "2015-07-30T16:16:10.000Z",
+    mtime: "2015-07-30T16:16:15.000Z",
+    ctime: "2015-07-30T16:18:51.000Z",
+    createdAt: "2015-07-31T14:20:40.384Z",
+    updatedAt: "2015-08-02T09:00:41.042Z",
+    id: 8
+  }]
+}, {
+  state: 'archived',
+  label: 'Nygade 1002',
+  address: 'Somewhere, Kopenhagen',
+  description: 'Reconstruction',
+
+  physicalAssets: [],
+  digitalObjects: [],
+
+  config: {
+    sda: {
+      topics: ["Haus 30 (general context)", "Haus 30 (political context)"]
+    },
+    geometricenrichment: {
+      tools: ["IFC Reconstruction", "Electrical Appliance Detection"]
+    }
+  },
+
+  files: [{
+    path: "/duraark-storage/sessions/byg72-2nd-scan_fixed/master/Nygade_Scan1002.e57",
+    type: "e57",
+    size: 269388800,
+    directory: false,
+    atime: "2015-07-30T16:16:10.000Z",
+    mtime: "2015-07-30T16:16:15.000Z",
+    ctime: "2015-07-30T16:18:51.000Z",
+    createdAt: "2015-07-31T14:20:40.384Z",
+    updatedAt: "2015-08-02T09:00:41.042Z",
+    id: 9
+  }]
+}, {
+  state: 'archived',
+  label: 'Nygade 1005-1006',
+  address: 'Somewhere, Kopenhagen',
+  description: 'Reconstruction',
+
+  physicalAssets: [],
+  digitalObjects: [],
+
+  config: {
+    sda: {
+      topics: ["Haus 30 (general context)", "Haus 30 (political context)"]
+    },
+    geometricenrichment: {
+      tools: ["IFC Reconstruction", "Electrical Appliance Detection"]
+    }
+  },
+
+  files: [{
+    path: "/duraark-storage/sessions/byg72-2nd-scan_fixed/master/Nygade_Scan1005-1006.e57",
+    type: "e57",
+    size: 538401792,
+    directory: false,
+    atime: "2015-07-30T16:16:10.000Z",
+    mtime: "2015-07-30T16:16:15.000Z",
+    ctime: "2015-07-30T16:18:51.000Z",
+    createdAt: "2015-07-31T14:20:40.384Z",
+    updatedAt: "2015-08-02T09:00:41.042Z",
+    id: 10
   }]
 }];
