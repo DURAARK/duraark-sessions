e86cfd2b074a25f5b8d5cd131dd6812a5fd01161
Martin Hecher <martin.hecher@fraunhofer.at>
2016-02-25 11:31:23 +0000

    added 'downloadURL' to file models

--- api/models/Files.js
+++ api/models/Files.js
@@ -11,6 +11,7 @@ module.exports = {
         path: 'string',
         type: 'string',
         size: 'integer',
+        downloadURL: 'string',
         atime: 'date',
         mtime: 'date',
         ctime: 'date'
